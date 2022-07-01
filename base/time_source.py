61d2e57ec038d1b97292f3611b7db5c4cee68315
KyunghoKimKurlyCorpCom <kyungho.kim@kurlycorp.com>
2022-07-02 01:19:18 +0900

    rft: Change TimeSource from interface to class again, and change name to Subject
      - ClassDiagram - Change name TimeSource to Subject
      - This will be applied Observer pattern, so change name TimeSource to Subject
      - Rename some instance method for readability

--- base/time_source.py
+++ base/time_source.py
@@ -1,11 +1,13 @@
-from abc import ABCMeta, abstractmethod
-
 from base.clock_observer import ClockObserver
 
 
-class TimeSource(metaclass=ABCMeta):
-    @abstractmethod
+class Subject:
+    def __init__(self):
+        self.__its_observers = list()
+
     def register_observer(self, observer: ClockObserver):
-        pass
+        self.__its_observers.append(observer)
 
-
+    def notify_observers(self, hours, minutes, seconds):
+        for observer in self.__its_observers:
+            observer.update(hours, minutes, seconds)
--- tests/mock_time_source.py
+++ tests/mock_time_source.py
@@ -1,15 +1,6 @@
-from base.clock_observer import ClockObserver
-from base.time_source import TimeSource
-from time_source_implementation import TImeSourceImplementation
+from base.time_source import Subject
 
 
-class MockTimeSource(TimeSource):
-    def __init__(self):
-        self.ts_imp = TImeSourceImplementation()
-
-    def register_observer(self, observer: ClockObserver):
-        self.ts_imp.register_observer(observer)
-
+class MockTimeSource(Subject):
     def set_time(self, hours, minutes, seconds):
-        self.ts_imp.notify(hours, minutes, seconds)
-
+        self.notify_observers(hours, minutes, seconds)
