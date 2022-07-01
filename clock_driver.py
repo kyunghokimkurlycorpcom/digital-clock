ebeb9ec4d29cb6229714b2b4ed3a68a1bdeb0faa
KyunghoKimKurlyCorpCom <kyungho.kim@kurlycorp.com>
2022-07-01 16:19:18 +0900

    rft: Make TimeSource can use other clock_observer
      - ClassDiagram - Breaking the dependency of TimeSource upon ClockDriver
      - Extract base class from ClockDriver - ClockObserver
      - Decouple dependency to ClockDriver in TimeSource, MockTimeSource
      - Rename instance variables that contains driver to observer

--- clock_driver.py
+++ clock_driver.py
@@ -1,6 +1,11 @@
-class ClockDriver:
-    def __init__(self, source, sink):
-        source.set_driver(self)
+from base.clock_observer import ClockObserver
+from base.time_sink import TimeSink
+from base.time_source import TimeSource
+
+
+class ClockDriver(ClockObserver):
+    def __init__(self, source: TimeSource, sink: TimeSink):
+        source.set_observer(self)
         self.its_sink = sink
 
     def update(self, hours, minutes, seconds):
